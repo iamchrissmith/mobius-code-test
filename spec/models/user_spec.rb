f52af57c99bf7f05522ca6c946045c63796bf159
Chris Smith <me@chrisryansmith.com>
2017-10-07 12:49:34 -0600

    validate user model

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,6 +5,7 @@ RSpec.describe User, type: :model do
     context "invalid attributes" do
       it { should have_secure_password }
       it { should validate_presence_of(:email) }
+      it { should validate_uniqueness_of(:email) }
     end
 
     context "valid attributes" do
