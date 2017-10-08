7368a9ed83c8126d10a41d258150f13a8e09cbac
Chris Smith <me@chrisryansmith.com>
2017-10-08 16:59:19 -0600

    add initial balance calculation for user

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,18 @@ class User < ApplicationRecord
 
   has_many :transactions
   has_many :receipts, :class_name => "Transaction", foreign_key: 'to'
+
+  def balance 
+    100 - credit_sent + credit_received
+  end
+
+  private
+
+    def credit_sent
+      transactions.sum(:amount)
+    end
+
+    def credit_received
+      receipts.sum(:amount)
+    end
 end
